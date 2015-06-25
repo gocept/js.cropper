d8071e67c5f71672a6620e6b47f038c37b939095
Florian Pilz <fp@gocept.com>
2015-06-25 11:19:48 +0200

    Add JS and CSS files for Cropper.

--- js/cropper/__init__.py
+++ js/cropper/__init__.py
@@ -1,7 +1,6 @@
 from fanstatic import Library, Resource
 
 library = Library('cropper', 'resources')
-
-# Define the resources in the library like this.
-# For options and examples, see the fanstatic documentation.
-# resource1 = Resource(library, 'style.css')
+cropper_css = Resource(library, 'cropper.css', minified='cropper.min.css')
+cropper = Resource(
+    library, 'cropper.js', minified='cropper.min.js', depends=[cropper_css])
